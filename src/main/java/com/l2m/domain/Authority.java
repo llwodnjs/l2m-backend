8ba43edf4819951edec88f307476a75432e51868
llwodnjs <llwodnjs@naver.com>
2023-02-21 09:31:08 +0900

    [수정] 권한명 수정
    [수정] 권한명 수정

--- src/main/java/com/l2m/domain/Authority.java
+++ src/main/java/com/l2m/domain/Authority.java
@@ -70,7 +70,7 @@ public class Authority extends BaseEntity {
     public static Supplier<Authority> createAuth(Member member) {
         final Authority authority = Authority.builder()
                                             .businessKey(BusinessKeyUtil.create(DomainPrefix.AUTH))
-                                            .name("ROLE_USER")
+                                            .name("USER")
                                             .member(member)
                                             .build();
         return () -> authority;
